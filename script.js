14a1695621000d135f019049d2a27a8cf64518a2
Tom Boyer <boyer.tom21@gmail.com>
2022-11-13 20:16:12 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// animation d'une img au scroll de la page --> translate + opacity
+// https://www.youtube.com/watch?v=SkheRkXbQuQ&ab_channel=LeDesignerduWeb-%C3%89coleduWeb
+
+const slidingNewsletter = document.querySelector(".slide-in");
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, clientHeight } = document.documentElement;
+
+  const topElementToTopViewport = slidingNewsletter.getBoudingClientRect().top;
+
+  if (
+    scrollTop >
+    (scrollTop + topElementToTopViewport).toFixed() - clientHeight * 0.7
+  ) {
+    slidingNewsletter.classList.add("active");
+  }
+});
